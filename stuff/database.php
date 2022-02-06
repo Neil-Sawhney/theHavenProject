747dbee00791efba3242fbc395af84778143312f
Neil Sawhney <Neil@neil-sawhney.com>
2022-02-06 03:19:01 +0000

    search field is in the right page, isnt showing results properly

--- stuff/database.php
+++ stuff/database.php
@@ -6,10 +6,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Database</title>
 </head>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 
 <body>
     <img id="topbannerDB" alt="this is the top banner" src="./xd-ref/topbanner.png">
     <div id="subtitle">Corporate Social Responsibility In-Kind Database</div>
+
+<div class="searchWrapper">
+
+<script>
+  //search code
+$(document).ready(function(){
+    $('.search-box input[type="text"]').on("keyup input", function(){
+        /* Get input value on change */
+        var inputVal = $(this).val();
+        var resultDropdown = $(this).siblings(".result");
+        if(inputVal.length){
+            $.get("backend-search.php", {term: inputVal}).done(function(data){
+                // Display the returned data in browser
+                resultDropdown.html(data);
+            });
+        } else{
+            resultDropdown.empty();
+        }
+    });
+    
+    // Set search input value on click of result item
+    $(document).on("click", ".result p", function(){
+        $(this).parents(".search-box").find('input[type="text"]').val($(this).text());
+        $(this).parent(".result").empty();
+    });
+});
+//end of search code
+
+
+</script>
+</head>
+<body>
+    <div class="search-box">
+        <input type="text" autocomplete="off" placeholder="Search category..." />
+        <div class="result"></div>
+    </div>
+</body>
+</html>
+</div>
+
 
          <?php
          echo "<div class='buttonWrapper'> ";
